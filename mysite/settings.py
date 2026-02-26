28bd57dbb46cea533c5cae1e2786e5e50711b0d7
Monisha <monishass636655@gmail.com>
2026-02-26 15:09:37 +0530

    Ready for Render deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0c40n_jawno*h+6h%jyh=_a$!)ukaplr2j@6njjzcdxv%07oc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
